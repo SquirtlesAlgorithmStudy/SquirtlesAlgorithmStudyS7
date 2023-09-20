693c8747d384e1ab5bc146c32732e0e1e715df9e
caneter <143891381+caneter@users.noreply.github.com>
2023-09-20 11:20:18 +0900

    [#4]

--- 예준/2주차/1931_회의실배정.py
+++ 예준/2주차/1931_회의실배정.py
@@ -1,4 +1,13 @@
 n = int(input())
 meeting = [list(map(int, input().split())) for _ in range(n)]
-meeting.sort()
-print(meeting)
+meeting.sort(key=lambda x:(x[1], x[0]))
+
+max_mt=0
+position = 0
+
+for start in range(n):
+    if(meeting[start][0] >= position):
+        position = meeting[start][1]
+        max_mt += 1
+
+print(max_mt)
