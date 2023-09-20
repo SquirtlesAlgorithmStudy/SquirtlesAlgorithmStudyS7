a5369acc79208a4738f70df0c754fe01c17937af
caneter <143891381+caneter@users.noreply.github.com>
2023-09-20 11:26:29 +0900

    [#4]

--- 예준/2주차/18310_안테나.py
+++ 예준/2주차/18310_안테나.py
@@ -3,7 +3,7 @@ import math
 n = int(input())
 house_location = list(map(int,input().split()))
 
-mean_loc= math.ceil(n/2)
-house_location.sort()
+mean_loc= math.ceil(n/2) # 전체 개수의 중간 위치 + 1 값
+house_location.sort() # 정렬
 
 print(house_location[mean_loc-1])
