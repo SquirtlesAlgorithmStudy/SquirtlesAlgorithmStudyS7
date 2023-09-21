acd350ed57ba937e6ae5b6d172e04b5d72405fe9
huijunam <aaa3k5@naver.com>
2023-09-21 13:56:48 +0900

    [#5] 18310 안테나 풀이

---
+++ 희주/그리디 알고리즘/18310_안테나.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+N = int(input())
+L = list(map(int,sys.stdin.readline().split()))
+result =[]
+L.sort(reverse=False) # 집의 위치 오름차순 정렬
+
+curr = int(len(L)/2) # 중앙 인덱스 
+heapq.heappush(result, [sum([abs(l-L[curr]) for l in L]), L[curr]]) # 중앙에서의 거리 합
+heapq.heappush(result, [sum([abs(l-L[curr-1]) for l in L]), L[curr-1]]) # 집의 갯수가 짝수일 경우 더 작은 인덱스 위치 찾기 위해
+
+sum_length, i = heapq.heappop(result)
+
+print(i)
