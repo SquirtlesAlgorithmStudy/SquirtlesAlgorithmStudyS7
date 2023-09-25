2172b44242677177fe015ee8b443be35f281e86d
정은재 <hackurs@naver.com>
2023-09-25 19:13:10 +0900

    [#5] 18310 풀이 완료

--- 은재/2주차/18310 안테나.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-input = sys.stdin.readline
-
-N = int(input())
-arr = list(map(int, input().split(' ')))
-
-arr.sort()
-print(arr[(N-1)//2])
---
+++ 은재/2주차/18310_안테나.py
@@ -0,0 +1,10 @@
+house_num = int(input())
+print("집의 수", house_num)
+house_loc = input()
+print("집의 위치", house_loc)
+
+house_loc = list(map(int, house_loc.split(' '))) #문자열 분리 및 정수 변환 후 리스트 생성
+house_loc.sort() #오름차순 정렬
+print(house_loc)
+
+# print(house_loc[(house_num-1)//2])
