a0ce9674319c3a07f85c968be2ce3ed7258215ba
dongdaejin1998 <35069745+dongdaejin1998@users.noreply.github.com>
2023-09-27 16:03:41 +0900

    [#7]

--- 동규/스택/4949.py
+++ 동규/스택/4949.py
@@ -3,13 +3,15 @@ import sys
 result_list=[]
 
 while True:
-    sentences =sys.stdin.readline().strip()
-    bracket_list=[]
+    sentences =sys.stdin.readline().rstrip()
+    if sentences==".":
+        break
+    bracket_list=["."]
     for sentence in sentences:
-        if sentence=="(" | sentence=="[":
+        if sentence=="(" or sentence=="[":
             bracket_list.append(sentence)
         if sentence==")":
-            if bracket_list[-1]=="(":
+            if bracket_list[-1] =="(" :
                 bracket_list.pop()
                 continue
             else:
@@ -22,9 +24,12 @@ while True:
             else:
                 result_list.append("no")
                 break
-        if sentence==".":
+        if len(bracket_list)==1 and sentence==".":
             result_list.append("yes")
-        #print(bracket_list)
+            continue
+        elif len(bracket_list)!=1 and sentence==".":
+            result_list.append("no")
+            break
 
 for result in result_list:
     print(result)
