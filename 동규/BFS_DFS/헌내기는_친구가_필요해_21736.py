979fa4f6562a9feb1ce883fa8f94bcfc625089ee
dongdaejin1998 <35069745+dongdaejin1998@users.noreply.github.com>
2023-10-24 16:36:10 +0900

    [#13]

--- 동규/BFS_DFS/헌내기는_친구가_필요해_21736.py
+++ 동규/BFS_DFS/헌내기는_친구가_필요해_21736.py
@@ -4,7 +4,7 @@ import sys
 N, M = map(int,sys.stdin.readline().split())
 campus=[]
 start=()
-result=0
+
 
 directions=[(0,1),(1,0),(0,-1),(-1,0)]
 
@@ -15,23 +15,27 @@ for row in range(N):
             start=(row,col)
     campus.append(cols)
         
-
 visited=[[False]*M for n in range(N)]
 queue=deque([start])
+visited[start[0]][start[1]]=True
 
 def dfs(queue):
+    result=0
     while queue:
         x,y=queue.popleft()
-        print(x,y)
         for dx,dy in directions:
             nx,ny=x+dx,y+dy
-            
-            if 0<= nx <M and 0<=ny<N:
+            if 0<= nx <N and 0<=ny<M:
+                if campus[nx][ny]=="P" and not visited[nx][ny]:
+                    result+=1
                 if campus[nx][ny]!="X" and not visited[nx][ny]:
+                    visited[nx][ny]=True
                     queue.append((nx,ny))
-
-            if campus[nx][ny]=="P":
-                result+=1
     return result
-print(dfs(queue))
+
+res=dfs(queue)
+if res>0:
+    print(res)
+else:
+    print("TT")
 
