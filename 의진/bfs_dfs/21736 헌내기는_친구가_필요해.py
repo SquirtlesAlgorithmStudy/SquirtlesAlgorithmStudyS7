b409cdca8e6d881a3fc42ec44d9ac5067a7a765d
lee-euiji <leeuj9663@naver.com>
2023-10-27 13:06:15 +0900

    [ADMIM] 6주차 문제풀이

---
+++ 의진/bfs_dfs/21736 헌내기는_친구가_필요해.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(N)]
+
+
+def bfs(r, c):
+    answer = 0
+    dr = [1, -1, 0, 0]
+    dc = [0, 0, 1, -1]
+
+    q = deque()
+    q.append((r, c))
+    while q:
+        r, c = q.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M:
+                if board[nr][nc] == "X":
+                    continue
+                elif board[nr][nc] == "P":
+                    answer += 1
+                board[nr][nc] = "X"
+                q.append((nr, nc))
+    return answer if answer != 0 else "TT"
+
+
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == "I":
+            print(bfs(i, j))
+            break
---
+++ 의진/bfs_dfs/2660_회장뽑기.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+member_num = int(input())
+graph = [[] for _ in range(member_num + 1)]
+while True:
+    edge = tuple(map(int, input().split()))
+    if edge == (-1, -1):
+        break
+    graph[edge[0]].append(edge[1])
+    graph[edge[1]].append(edge[0])
+
+
+def bfs(start):
+    q = deque()
+    q.append((start, 0))
+    visited = [False] * (member_num + 1)
+    result = 0
+    visited[start] = True
+    while q:
+        member = q.popleft()
+        result = max(member[1], result)
+        for neighbor in graph[member[0]]:
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                q.append((neighbor, member[1] + 1))
+    return result
+
+
+score_list = [member_num + 1] + ([0] * member_num)
+for i in range(1, member_num + 1):
+    score_list[i] = bfs(i)
+
+candidate = []
+min_score = min(score_list)
+for i, score in enumerate(score_list):
+    if score == min_score:
+        candidate.append(i)
+print(min_score, len(candidate))
+print(" ".join(map(str, candidate)))
