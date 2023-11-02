1d12d6da8cfafcb5e34fc909e7e1d14247c12bd9
dongdaejin1998 <35069745+dongdaejin1998@users.noreply.github.com>
2023-11-02 15:48:08 +0900

    [#15]

--- 동규/BFS_DFS/숨바꼭질_1697.py
+++ 동규/BFS_DFS/숨바꼭질_1697.py
@@ -3,23 +3,17 @@ from collections import deque
 
 N,K= map(int,sys.stdin.readline().split())
 queue=deque([N])
-directions=[-1,1,2]
-time=1
-count=0
-while queue:
-    X=queue.popleft()
-    
-    for direction in directions:
-        if direction==1 or direction==-1:
-            queue.append(X+direction)
-        elif direction==2:
-            queue.append(X*2)
-        count+=1
-    print(count)
-    print(queue)
-    if K in queue:
-        print(time)
-        break
-    if count==time*3:
-        time+=1
-        count=0
+dist=[0]*100001
+
+def dfs():
+    while queue:
+        X=queue.popleft()
+        if X==K:
+            print(dist[X])
+            break
+        for dX in (X+1,X-1,X*2):
+            if 0<=dX<=100000 and not dist[dX]:
+                
+                dist[dX]=dist[X]+1
+                queue.append(dX)
+dfs()
