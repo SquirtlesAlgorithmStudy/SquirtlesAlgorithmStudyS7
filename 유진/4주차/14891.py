cbc4754290caff712d2109360bef28decc073676
lee-euiji <leeuj9663@naver.com>
2023-11-10 09:24:37 +0900

    [ADMIM] 9주차 문제풀이

--- 유진/4주차/14891.py
+++ 유진/4주차/14891.py
@@ -1,4 +1,5 @@
-import sys, collections
+import sys
+import collections
 s = []
 for _ in range(4):
     s.append(collections.deque(list(input())))
@@ -7,26 +8,31 @@ for _ in range(4):
 K = int(sys.stdin.readline())
 R = [list(map(int, sys.stdin.readline().split())) for _ in range(K)]
 
-#왼쪽 톱니바퀴 확인
-def left(num, direction): 
+# 왼쪽 톱니바퀴 확인
+
+
+def left(num, direction):
     if num < 0:
-        return 
+        return
     if s[num][2] != s[num+1][6]:
         left(num-1, -direction)
         s[num].rotate(direction)
 
-#오른쪽 톱니바퀴 확인
-def right(num, direction): 
+# 오른쪽 톱니바퀴 확인
+
+
+def right(num, direction):
     if num > 3:
         return
     if s[num][6] != s[num-1][2]:
         right(num+1, -direction)
         s[num].rotate(direction)
 
+
 for i in range(K):
     num = R[i][0] - 1
     direction = R[i][1]
-    
+
     left(num-1, -direction)
     right(num+1, -direction)
     s[num].rotate(direction)
@@ -41,5 +47,5 @@ if s[2][0] == '1':
     res += 4
 if s[3][0] == '1':
     res += 8
-    
+
 print(res)
--- 유진/6주차/2660.py
+++ 유진/6주차/2660.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 
+
 def bfs(start):
     queue = deque([[start, 0]])
     while queue:
@@ -10,12 +11,14 @@ def bfs(start):
                 queue.append([v, dist+1])
                 visited[v] = dist+1
 
+
 def get_max_dist():
     max_dist = 0
     for dist in visited:
         if dist > max_dist:
             max_dist = dist
     return max_dist
+
 
 n = int(sys.stdin.readline().strip())
 
@@ -27,9 +30,9 @@ while True:
         break
     member[mem1].append(mem2)
     member[mem2].append(mem1)
-    
 
-result = [51 for _ in range(n+1)] 
+
+result = [51 for _ in range(n+1)]
 for i in range(1, n+1):
     visited = [0 for _ in range(n+1)]
     visited[i] = 1
@@ -37,7 +40,7 @@ for i in range(1, n+1):
     result[i] = get_max_dist()
 
 min_dist = min(result)
-candis = [i for i in range(0,len(result)) if result[i] == min_dist]
+candis = [i for i in range(0, len(result)) if result[i] == min_dist]
 
 print(min_dist, len(candis))
-print(*candis) 
+print(*candis)
