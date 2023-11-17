3835386e02b50816dd0c701d4e5c794a571f1494
lee-euiji <leeuj9663@naver.com>
2023-11-17 13:59:32 +0900

    [ADMIN] 10주차 미션 문제 풀이

--- 의진/playground.py
+++ 의진/playground.py
@@ -1,30 +1,17 @@
 import time
 dict = {i: i for i in range(10_000_000)}
-
+# 1
 start = time.time()
 count = 0
-for _ in range(10_000_200):
-    if 9_999_999 in dict.keys():
+for i in range(10_000_000):
+    if i in list(dict.keys()):
         count += 1
 print(time.time() - start)
 
+# 2
 start = time.time()
 count = 0
-for _ in range(10_000_200):
-    if dict.get(9_999_999) is not None:
-        count += 1
-print(time.time() - start)
-
-start = time.time()
-count = 0
-for i in range(10_000_200):
-    if i in dict.keys():
-        count += 1
-print(time.time() - start)
-
-start = time.time()
-count = 0
-for i in range(10_000_200):
+for i in range(10_000_000):
     if dict.get(i) is not None:
         count += 1
 print(time.time() - start)
---
+++ 의진/이진탐색/16434_드래곤_앤_던전_이진탐색.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+N, h_atk = map(int, input().split())
+rooms = [tuple(map(int, input().split())) for _ in range(N)]
+
+
+def simulation(h_max, h_atk):
+    cur_h_atk = h_atk
+    cur_hp = h_max
+    for room in rooms:
+        if room[0] == 1:
+            attack_count = (room[2] // cur_h_atk) + min(1, room[2] % cur_h_atk)
+            cur_hp -= (room[1] * (attack_count - 1))
+            if cur_hp <= 0:
+                return False
+        elif room[0] == 2:
+            cur_h_atk += room[1]
+            cur_hp += room[2]
+            cur_hp = min(cur_hp, h_max)
+    return True
+
+
+start = 1
+end = 1_000_000 * 1_000_000 * 123_456
+
+while (start <= end):
+    mid = (start + end) // 2
+    if simulation(mid, h_atk):
+        end = mid - 1
+    else:
+        start = mid + 1
+print(start)
